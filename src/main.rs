2b9bd085cc5e9eef06e0133528a9c6a1cf308db0
eriksterneberg <erik.sterneberg@gmail.com>
2024-02-17 21:22:45 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -8,86 +8,85 @@ use std::collections::HashMap;
 mod strategies;
 
 fn main() {
-    let mut strategies: Vec<Box<dyn Strategy>> = vec![
-        // Box::new(AlwaysCooperate::new()),
-        Box::new(TitForTat::new()),
-        Box::new(TitFor2Tats::new()),
-        Box::new(AlwaysDefect::new()),
-    ];
-
-    let mut strategies2: Vec<Box<dyn Strategy>> = vec![
-        // Box::new(AlwaysCooperate::new()),
-        Box::new(TitForTat::new()),
-        Box::new(TitFor2Tats::new()),
-        Box::new(AlwaysDefect::new()),
-    ];
-
-    // for mut strategy in &strategies {
-    //     println!(
-    //         "Initial action for strategy {} is {:?}",
-    //         strategy.name(),
-    //         strategy.get()
-    //     );
-    //
-    //     strategy.put(&Action::Defect);
-    //
-    //     println!("After defecting, next move is {:?}", strategy.get());
-    // }
-
+    let mut strategies = get_strategies();
+    let mut strategies2 = get_strategies();
     let mut scores: HashMap<String, i32> = HashMap::new();
 
     println!("Starting tournament");
 
-    for mut i in &mut strategies {
-        for mut j in &mut strategies2 {
-            // if i.name() == j.name() {
-            //     continue;
-            // }
-
-            println!("Executing battle: {} vs {}", i.name(), j.name());
-
-            for _ in 0..200 {
-                let action = i.get();
-                let reaction = j.get();
-                i.put(&reaction);
-                j.put(&action);
-
-                match (action, reaction) {
-                    (Action::Cooperate, Action::Cooperate) => {
-                        println!("Both players cooperated! Will assign both 3 points each.");
-                        add_score(&mut scores, i.name(), 3);
-                        add_score(&mut scores, j.name(), 3);
-                    }
-                    (Action::Defect, Action::Defect) => {
-                        println!("Both defected, so both get a single point.");
-                        add_score(&mut scores, i.name(), 1);
-                        add_score(&mut scores, j.name(), 1);
-                    }
-                    (Action::Cooperate, Action::Defect) => {
-                        println!("One defected and gets 5, one cooperated and gets zero");
-                        add_score(&mut scores, j.name(), 5);
-                    }
-                    (Action::Defect, Action::Cooperate) => {
-                        println!("One defected and gets 5, one cooperated and gets zero");
-                        add_score(&mut scores, i.name(), 5);
-                    }
-                }
-            }
+    for i in &mut strategies {
+        for j in &mut strategies2 {
+            battle(i, j, &mut scores);
         }
     }
 
     println!("Tournament finished");
 
-    // I have a hashmap with values and scores. I want to sort by scores and print the results
+    print_scores(&scores);
+}
+
+fn battle(i: &mut Box<dyn Strategy>, j: &mut Box<dyn Strategy>, scores: &mut HashMap<String, i32>) {
+    println!("Executing battle: {} vs {}", i.name(), j.name());
+
+    for _ in 0..200 {
+        let action = i.get();
+        let reaction = j.get();
+        i.put(&reaction);
+        j.put(&action);
+
+        match (action, reaction) {
+            (Action::Cooperate, Action::Cooperate) => {
+                println!("Both players cooperated! Will assign both 3 points each.");
+                add_score(scores, i.name(), 3);
+                add_score(scores, j.name(), 3);
+            }
+            (Action::Defect, Action::Defect) => {
+                println!("Both defected, so both get a single point.");
+                add_score(scores, i.name(), 1);
+                add_score(scores, j.name(), 1);
+            }
+            (Action::Cooperate, Action::Defect) => {
+                println!("One defected and gets 5, one cooperated and gets zero");
+                add_score(scores, j.name(), 5);
+            }
+            (Action::Defect, Action::Cooperate) => {
+                println!("One defected and gets 5, one cooperated and gets zero");
+                add_score(scores, i.name(), 5);
+            }
+        }
+    }
+}
+
+/// Get all strategies
+///
+/// It is not possible to copy an array, so instead we return a vector of boxes.
+/// Why boxes? Because we want to return a vector of different types,
+/// and the only way to do that is to use a trait object.
+///
+/// A trait object is a pointer to a trait, which can be used to call the methods of the trait.
+/// In this case, we are returning a vector of boxes that contain a trait object.
+/// This means that we can call the methods of the trait on the objects in the vector.
+/// The trait object is a pointer to the trait, and the box is a pointer to the object.
+/// This is the only way to return a vector of different types.
+fn get_strategies() -> Vec<Box<dyn Strategy>> {
+    vec![
+        Box::new(AlwaysCooperate::new()),
+        Box::new(TitForTat::new()),
+        Box::new(TitFor2Tats::new()),
+        Box::new(AlwaysDefect::new()),
+    ]
+}
+
+fn add_score(scores: &mut HashMap<String, i32>, player: String, score: i32) {
+    let entry = scores.entry(String::from(player)).or_insert(0);
+    *entry += score;
+}
+
+fn print_scores(scores: &HashMap<String, i32>) {
     let mut scores: Vec<_> = scores.into_iter().collect();
     scores.sort_by(|a, b| b.1.cmp(&a.1));
 
     for (player, score) in &scores {
         println!("{}\t{}", score, player);
     }
-}
-
-fn add_score(scores: &mut HashMap<String, i32>, player: String, score: i32) {
-    let entry = scores.entry(String::from(player)).or_insert(0);
-    *entry += score;
 }
