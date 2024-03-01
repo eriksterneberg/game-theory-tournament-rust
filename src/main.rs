3436395e493bbf8e84ff34ab1e16962d7049a39d
eriksterneberg <erik.sterneberg@gmail.com>
2024-03-01 23:24:22 +0100

    Allow cancelling mid-battle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread::sleep;
 use strategies::enums::{get_strategies, StrategyEnum};
 use crate::scoreboard::Scoreboard;
@@ -8,8 +10,7 @@ use itertools::iproduct;
 use crate::types::{Parameters, Score};
 use log::{info, trace};
 use anyhow::{Result};
-use crossbeam_channel::{bounded, tick, Receiver, select};
-use crossbeam_channel::internal::select;
+use crossbeam_channel::{bounded, Receiver, select};
 use ctrlc;
 
 mod scoreboard;
@@ -21,33 +22,21 @@ mod tests;
 fn main() -> Result<()> {
     logging::init()?;
     let parameters = Parameters::parse();
-
-    // Keep a list of the scores
+    let running = get_cancel();
     let mut board = Scoreboard::default();
 
     info!("Starting tournament");
 
-    let ctrl = ctrl_channel();
-
-    println!("Starting tournament, sleeping. Please wait...");
-    // sleep(std::time::Duration::from_millis(10000));
-
     // All strategies battle all strategies
     for (i, j) in iproduct!(get_strategies(), get_strategies()) {
-        select! {
-            recv(ctrl) -> _ => {
-                println!("Battle interrupted");
-                std::process::exit(0);
-            },
-            default => {}
-        }
+        check_running(&running)?;
 
         if i == j {
             trace!("Skipping self battle");
             continue;
         }
 
-        let (i_score, j_score) = battle(i, j, parameters.iterations, parameters.verbose.is_present());
+        let (i_score, j_score) = battle(Some(running.clone()), i, j, parameters.iterations, parameters.verbose.is_present());
         board.add_score(i, i_score);
         board.add_score(j, j_score);
     }
@@ -61,20 +50,34 @@ fn main() -> Result<()> {
 
 
 /// Executes battle between two strategies
-fn battle(i_enum: StrategyEnum, j_enum: StrategyEnum, iterations: i64, verbose: bool) -> (Score, Score) {
+fn battle(running: Option<Arc<AtomicBool>>, i_enum: StrategyEnum, j_enum: StrategyEnum, iterations: i64, verbose: bool) -> (Score, Score) {
 
     // Create strategies
     let (mut i, mut j) = (get_strategy(i_enum), get_strategy(j_enum));
 
     trace!("Starting battle {:?} vs {:?}", i_enum, j_enum);
 
-
     let bar = ProgressBar::new(iterations as u64);
     bar.set_style(ProgressStyle::default_bar()
         .template("{spinner:.red} [{elapsed_precise}] [{bar:40.red/pink}] {percent}% {msg}").unwrap());
 
     // Fold over the range of iterations to accumulate scores
-    let scores = (0..iterations).fold((0, 0), |(i_score, j_score), _| {
+    let (mut i_score, mut j_score) = (0, 0);
+
+    for _ in 0..iterations {
+        match running {
+            Some(ref running) => {
+                match check_running(&running) {
+                    Ok(_) => {}
+                    Err(_) => {
+                        trace!("Cancelled in mid-battle");
+                        return (i_score, j_score);
+                    }
+                }
+            }
+            None => {}
+        }
+
         if verbose {
             bar.inc(1);
             bar.set_message(format!("{:?} vs {:?}", i_enum, j_enum));
@@ -82,31 +85,31 @@ fn battle(i_enum: StrategyEnum, j_enum: StrategyEnum, iterations: i64, verbose: 
         }
 
         // Players make moves independently
-        let (action_i, action_j) = (i.get(), j.get());
+        let (action, reaction) = (i.get(), j.get());
 
         // Players learn from the previous round
-        j.put(&action_i);
-        i.put(&action_j);
+        j.put(&action);
+        i.put(&reaction);
 
         // Score the round
-        let (i_, j_) = score(action_i, action_j);
+        let (i_, j_) = score(action, reaction);
 
-        // Return the updated scores
-        (i_score + i_, j_score + j_)
-    });
+        i_score += i_;
+        j_score += j_;
+    }
 
     if verbose {
         let prefix = format!("{:?} vs {:?}:", i_enum, j_enum);
-        if scores.0 > scores.1 {
+        if i_score > j_score {
             bar.finish_with_message(format!("{} {:?} won", prefix, i_enum));
-        } else if scores.0 < scores.1 {
+        } else if i_score < j_score {
             bar.finish_with_message(format!("{} {:?} won", prefix, j_enum));
         } else {
             bar.finish_with_message(format!("{} Draw", prefix));
         }
     }
 
-    scores
+    (i_score, j_score)
 }
 
 /// Scores the actions of two players
@@ -119,10 +122,43 @@ fn score(action: Action, reaction: Action) -> (Score, Score) {
     }
 }
 
-fn ctrl_channel() -> Receiver<()> {
-    let (tx, rx) = bounded(1);
+/// Creates a cancellation flag that can be used to gracefully terminate the program in response to a SIGINT signal (Ctrl+C).
+///
+/// This function sets up a signal handler for SIGINT signals using the `ctrlc` crate. When a SIGINT signal is received,
+/// the `running` flag is set to `false`, indicating that the program should terminate gracefully.
+///
+/// # Returns
+///
+/// * `Arc<AtomicBool>` - Returns an `Arc<AtomicBool>` representing the cancellation flag.
+fn get_cancel() -> Arc<AtomicBool> {
+    let (tx, _rx) = bounded(1);
+    let running = Arc::new(AtomicBool::new(true));
+    let running_ = running.clone();
     ctrlc::set_handler(move || {
+        running_.store(false, Ordering::SeqCst);
         let _ = tx.send(());
     }).expect("Error setting Ctrl-C handler");
-    rx
+    running
+}
+
+/// Checks whether the program is still running based on the value of the `running` flag.
+///
+/// This function takes a reference to an `Arc<AtomicBool>` named `running` as its argument.
+/// The `running` flag indicates whether the program should continue running.
+///
+/// # Arguments
+///
+/// * `running` - A reference to an `Arc<AtomicBool>` that represents the running flag.
+///
+/// # Returns
+///
+/// * `Result<()>` - Returns `Ok(())` if the program is still running (i.e., the `running` flag is `true`).
+///                  Returns `Err` with a message "Cancelled" if the program has been cancelled (i.e., the `running` flag is `false`).
+///
+fn check_running(running: &Arc<AtomicBool>) -> Result<()> {
+    if !running.load(Ordering::SeqCst) {
+        Err(anyhow::anyhow!("Cancelled"))
+    } else {
+        Ok(())
+    }
 }
--- src/tests/main_test.rs
+++ src/tests/main_test.rs
@@ -12,7 +12,7 @@ use crate::strategies::enums::StrategyEnum::{AlwaysDefect, TitFor2Tats, TitForTa
 /// but not before then.
 #[rstest(iterations, i_expected, j_expected, case(1, 0, 5), case(2, 0, 10), case(3, 1, 11))]
 fn test_battle_tit_for_2_tats_learns_slowly(iterations: i64, i_expected: i32, j_expected: i32) {
-    assert_eq!(battle(TitFor2Tats, AlwaysDefect, iterations, false), (i_expected, j_expected));
+    assert_eq!(battle(None, TitFor2Tats, AlwaysDefect, iterations, false), (i_expected, j_expected));
 }
 
 /// Test that the strategies TitForTat and TitFor2Tats always start to cooperate and continue
@@ -20,7 +20,7 @@ fn test_battle_tit_for_2_tats_learns_slowly(iterations: i64, i_expected: i32, j_
 #[rstest(iterations, i, j, case(1, 3, 3), case(10, 30, 30))]
 fn test_two_positive_strategies_always_cooperate(iterations: i64, i: i32, j: i32) {
     let expected = (i, j);
-    let results = battle(TitForTat, TitFor2Tats, iterations, false);
+    let results = battle(None, TitForTat, TitFor2Tats, iterations, false);
     assert_eq!(results, expected);
 }
 
